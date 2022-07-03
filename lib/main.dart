37bc5101e8bd8a04e2acc5a2423b6026d0a4b686
4geru <westhouse51@gmail.com>
2022-07-03 10:19:03 +0900

    simple flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-
-import './indicator.dart';
-import './utils.dart';
 
 void main() {
   runApp(const MyApp());
@@ -79,31 +75,30 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            PieChartSample1(),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('here is message'),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -111,179 +106,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class PieChartSample1 extends StatefulWidget {
-  const PieChartSample1({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => PieChartSample1State();
-}
-
-class PieChartSample1State extends State {
-  int touchedIndex = -1;
-
-  @override
-  Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.3,
-      child: Card(
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 28,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Indicator(
-                  color: const Color(0xff0293ee),
-                  text: 'One',
-                  isSquare: false,
-                  size: touchedIndex == 0 ? 18 : 16,
-                  textColor: touchedIndex == 0 ? Colors.black : Colors.grey,
-                ),
-                Indicator(
-                  color: const Color(0xfff8b250),
-                  text: 'Two',
-                  isSquare: false,
-                  size: touchedIndex == 1 ? 18 : 16,
-                  textColor: touchedIndex == 1 ? Colors.black : Colors.grey,
-                ),
-                Indicator(
-                  color: const Color(0xff845bef),
-                  text: 'Three',
-                  isSquare: false,
-                  size: touchedIndex == 2 ? 18 : 16,
-                  textColor: touchedIndex == 2 ? Colors.black : Colors.grey,
-                ),
-                Indicator(
-                  color: const Color(0xff13d38e),
-                  text: 'Four',
-                  isSquare: false,
-                  size: touchedIndex == 3 ? 18 : 16,
-                  textColor: touchedIndex == 3 ? Colors.black : Colors.grey,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: PieChart(
-                  PieChartData(
-                      pieTouchData: PieTouchData(touchCallback:
-                          (FlTouchEvent event, pieTouchResponse) {
-                        setState(() {
-                          if (!event.isInterestedForInteractions ||
-                              pieTouchResponse == null ||
-                              pieTouchResponse.touchedSection == null) {
-                            touchedIndex = -1;
-                            return;
-                          }
-                          touchedIndex = pieTouchResponse
-                              .touchedSection!.touchedSectionIndex;
-                        });
-                      }),
-                      startDegreeOffset: 180,
-                      borderData: FlBorderData(
-                        show: false,
-                      ),
-                      sectionsSpace: 1,
-                      centerSpaceRadius: 0,
-                      sections: showingSections()),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  List<PieChartSectionData> showingSections() {
-    return List.generate(
-      4,
-      (i) {
-        final isTouched = i == touchedIndex;
-        final opacity = isTouched ? 1.0 : 0.6;
-
-        const color0 = Color(0xff0293ee);
-        const color1 = Color(0xfff8b250);
-        const color2 = Color(0xff845bef);
-        const color3 = Color(0xff13d38e);
-
-        switch (i) {
-          case 0:
-            return PieChartSectionData(
-              color: color0.withOpacity(opacity),
-              value: 25,
-              title: '',
-              radius: 80,
-              titleStyle: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff044d7c)),
-              titlePositionPercentageOffset: 0.55,
-              borderSide: isTouched
-                  ? BorderSide(color: color0.darken(40), width: 6)
-                  : BorderSide(color: color0.withOpacity(0)),
-            );
-          case 1:
-            return PieChartSectionData(
-              color: color1.withOpacity(opacity),
-              value: 25,
-              title: '',
-              radius: 65,
-              titleStyle: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff90672d)),
-              titlePositionPercentageOffset: 0.55,
-              borderSide: isTouched
-                  ? BorderSide(color: color1.darken(40), width: 6)
-                  : BorderSide(color: color2.withOpacity(0)),
-            );
-          case 2:
-            return PieChartSectionData(
-              color: color2.withOpacity(opacity),
-              value: 25,
-              title: '',
-              radius: 60,
-              titleStyle: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff4c3788)),
-              titlePositionPercentageOffset: 0.6,
-              borderSide: isTouched
-                  ? BorderSide(color: color2.darken(40), width: 6)
-                  : BorderSide(color: color2.withOpacity(0)),
-            );
-          case 3:
-            return PieChartSectionData(
-              color: color3.withOpacity(opacity),
-              value: 25,
-              title: '',
-              radius: 70,
-              titleStyle: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff0c7f55)),
-              titlePositionPercentageOffset: 0.55,
-              borderSide: isTouched
-                  ? BorderSide(color: color3.darken(40), width: 6)
-                  : BorderSide(color: color2.withOpacity(0)),
-            );
-          default:
-            throw Error();
-        }
-      },
     );
   }
 }
